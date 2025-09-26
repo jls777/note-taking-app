4c2c5af5de024d479f11dfa6a7f35fac16258df1
John Sison <132543192+jls777@users.noreply.github.com>
2025-09-26 04:10:34 -0400

    added noteform component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
+import NewNote from "./NewNote";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>hello</h1>} />
-      <Route path="/new" element={<h1>new</h1>} />
-      <Route path="/:id">
-        <Route index element={<h1>show</h1>} />
-        <Route path="edit" element={<h1>edit</h1>} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>hello</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>show</h1>} />
+          <Route path="edit" element={<h1>edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 }
 
